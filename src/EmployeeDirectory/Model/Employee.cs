eefa292cd8d711083766531ab0ff57caea44cb54
Armando Martinez <jmtzpena@gmail.com>
2019-05-23 16:22:23 -0500

    Add SportTeam to Employee model

--- src/EmployeeDirectory/Model/Employee.cs
+++ src/EmployeeDirectory/Model/Employee.cs
@@ -6,6 +6,7 @@
         public string HashedPassword { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string SportTeam { get; set; }
         public string Title { get; set; }
         public Office Office { get; set; }
         public string PhoneNumber { get; set; }
